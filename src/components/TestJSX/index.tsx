a98a9bc3951f230f82b181f434df202f1e94ed9e
sakouksa <hjhfg7226@gmail.com>
2025-12-05 16:07:33 +0700

    Update code

--- src/components/TestJSX/index.tsx
+++ src/components/TestJSX/index.tsx
@@ -1,5 +1,16 @@
 const name = "ReactJS";
 const GreetingTitle = "This is ReactJS Title";
+//Const myobject = {title: "ReactJS Title"}; --- IGNORE ---
+interface myobject {
+  id: number;
+  name: string;
+  position?: string;
+}
+const myobject: myobject = {
+  id: 1,
+  name: "sakousa",
+  position: "Developer",
+};
 function TestJSX() {
   return (
     <>
@@ -7,6 +18,13 @@ function TestJSX() {
         Hello {name} {1 + 2}
       </h1>
       <h1 title={GreetingTitle}>Hello Javascript</h1>
+      <ul>
+        {" "}
+        <h1>Object Values:</h1>
+        <li> ID: {myobject["id"]}</li>
+        <li> Name: {myobject["name"].toUpperCase()}</li>
+        <li> Position: {myobject["position"]?.toLowerCase()}</li>
+      </ul>
     </>
   );
 }
